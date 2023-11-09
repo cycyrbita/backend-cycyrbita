4ad28a522d5e6744e530a6dca3acca115f5173e7
Никита <35835844+nikita-developer@users.noreply.github.com>
2023-11-09 13:24:24 +0300

    ингредиенты

--- models/ingredients/ingerdient-model.js
+++ models/ingredients/ingerdient-model.js
@@ -1,12 +1,12 @@
 const {Schema, model} = require('mongoose')
 
 const IngredientSchema = new Schema({
-  countrys: {type: Array, require: true, default: [{type: Schema.Types.ObjectId, ref: 'IngredientCountry'}]},
-  themes: {type: Array, require: true, default: [{type: Schema.Types.ObjectId, ref: 'IngredientTheme'}]},
-  titles: {type: Array, require: true, default: [{type: Schema.Types.ObjectId, ref: 'IngredientTitle'}]},
-  descriptions: {type: Array, require: true, default: [{type: Schema.Types.ObjectId, ref: 'IngredientDescription'}]},
-  tags: {type: Array, require: true, default: [{type: Schema.Types.ObjectId, ref: 'IngredientTag'}]},
-  images: {type: Array, require: true, default: [{type: Schema.Types.ObjectId, ref: 'IngredientImage'}]}
+  countrys: {type: Array},
+  themes: {type: Array},
+  titles: {type: Array},
+  descriptions: {type: Array},
+  tags: {type: Array},
+  images: {type: Array}
 })
 
 module.exports = model('Ingredient', IngredientSchema.set('timestamps', true))
