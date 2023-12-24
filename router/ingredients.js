640eab7a57f8bda31d7da67eb5ae02f7d4add120
nikita <nikitka.kuznetsov.1994@mail.ru>
2023-12-24 15:36:45 +0300

    изменил время жизни токенов и добавил middleware для ингредиентов

--- router/ingredients.js
+++ router/ingredients.js
@@ -1,12 +1,13 @@
 const ingredients = require('express').Router()
 const ingredientController = require('../controllers/ingredient-controller')
+const authMiddleware = require("../middleware/auth-middleware");
 
-ingredients.get('/get-ingredient-themes', ingredientController.getIngredientThemes)
-ingredients.post('/create-ingredient-theme', ingredientController.createIngredientTheme)
-ingredients.post('/create-ingredient', ingredientController.createIngredient)
-ingredients.post('/get-ingredients', ingredientController.getIngredients)
-ingredients.post('/get-ingredient', ingredientController.getIngredient)
-ingredients.delete('/deleted-ingredient', ingredientController.deletedIngredient)
-ingredients.post('/edit-ingredient', ingredientController.editIngredient)
+ingredients.get('/get-ingredient-themes', authMiddleware, ingredientController.getIngredientThemes)
+ingredients.post('/create-ingredient-theme', authMiddleware, ingredientController.createIngredientTheme)
+ingredients.post('/create-ingredient', authMiddleware, ingredientController.createIngredient)
+ingredients.post('/get-ingredients', authMiddleware, ingredientController.getIngredients)
+ingredients.post('/get-ingredient', authMiddleware, ingredientController.getIngredient)
+ingredients.delete('/deleted-ingredient', authMiddleware, ingredientController.deletedIngredient)
+ingredients.post('/edit-ingredient', authMiddleware, ingredientController.editIngredient)
 
 module.exports = ingredients
