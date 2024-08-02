63925c5cc3357f06929b0e685fc94e2febe108e6
m.gromov <cycyrbita@mail.ru>
2024-08-02 17:07:08 +0300

    https://trello.com/c/o7Zk8vcz - fix new-promo model

--- models/new-promo-model.js
+++ models/new-promo-model.js
@@ -4,7 +4,9 @@ const NewPromoSchema = new Schema({
     title: {type: String, require: true, default: 'Без названия'},
     lends: {type: Array, default: []},
     prelends: {type: Array, default: []},
+    sites: {type: Array, default: []},
     others: {type: Array, default: []},
+    dateChange: {type: Date, default: Date.now}
 })
 
 module.exports = model('NewPromo', NewPromoSchema.set('timestamps', true))
